9665543f0920b5b4759495df9957d6d93898190c
Nzhdeh <nzhdeh1991@gmail.com>
2019-10-17 12:45:47 +0200

    gfdrgf

--- ListViewPersonalizado/app/src/main/java/es/iesnervion/nyeghiazaryan/listviewpersonalizado/Clases/Persona.java
+++ ListViewPersonalizado/app/src/main/java/es/iesnervion/nyeghiazaryan/listviewpersonalizado/Clases/Persona.java
@@ -1,24 +1,61 @@
 package es.iesnervion.nyeghiazaryan.listviewpersonalizado.Clases;
 
 import android.media.Image;
+import android.widget.ImageView;
 
 public class Persona
 {
     private String nombre;
-    private String apellidos;
+   // private String apellidos;
     private int imagen;
 
     public Persona()
     {
         this.nombre="";
-        this.apellidos="";
+        //this.apellidos="";
         this.imagen=0;
     }
 
-    public Persona(String nombre,String apellidos,int imagen)
+    public Persona(String nombre, int imagen)
     {
         this.nombre=nombre;
-        this.apellidos=apellidos;
+        //this.apellidos=apellidos;
         this.imagen=imagen;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+   // public String getApellidos()
+//    {
+//        return apellidos;
+//    }
+
+    public int getImagen()
+    {
+        return imagen;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    //public void setApellidos(String apellidos)
+//    {
+//        this.apellidos = apellidos;
+//    }
+
+    public void setImagen(int imagen)
+    {
+        this.imagen = imagen;
+    }
+
+    @Override
+    public String toString()
+    {
+        return (getImagen()+","+getNombre());
     }
 }
--- ListViewPersonalizado/app/src/main/java/es/iesnervion/nyeghiazaryan/listviewpersonalizado/Clases/PersonaAdapter.java
+++ ListViewPersonalizado/app/src/main/java/es/iesnervion/nyeghiazaryan/listviewpersonalizado/Clases/PersonaAdapter.java
@@ -2,29 +2,62 @@ package es.iesnervion.nyeghiazaryan.listviewpersonalizado.Clases;
 
 import android.app.Person;
 import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class PersonaAdapter extends Adapter<Persona>
+import java.util.ArrayList;
+
+import es.iesnervion.nyeghiazaryan.listviewpersonalizado.R;
+
+public class PersonaAdapter extends BaseAdapter
 {
      Context contexto;
-     int layoutResourceID;
-     Persona [] personas=null;
+     ArrayList <Persona> personas=null;
 
-    public PersonaAdapter(Context contexto,int layoutResourceID,Persona [] personas)
+    public PersonaAdapter(Context contexto,ArrayList<Persona> personas)
     {
-        super(contexto,layoutResourceID,personas);
+        //super();
         this.contexto=contexto;
-        this.layoutResourceID=layoutResourceID;
         this.personas=personas;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return personas.size();
+    }
+
+    @Override
+    public Object getItem(int position)
+    {
+        return personas.get(position);
+    }
+
+    @Override
+    public long getItemId(int position)
+    {
+        return 0;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        View vista=convertView;
+        Persona p= (Persona) getItem(position);
+        convertView= LayoutInflater.from(contexto).inflate(R.layout.list_item_row,null);
 
-        return null;
+        ImageView foto=convertView.findViewById(R.id.imagen);
+        TextView nombre=convertView.findViewById(R.id.tv1);
+        //TextView apellidos=convertView.findViewById(R.id.tv2);
+
+        foto.setImageResource(p.getImagen());
+        nombre.setText(p.getNombre());
+        //apellidos.setText(p.getApellidos());
+
+        return convertView;
     }
 }
