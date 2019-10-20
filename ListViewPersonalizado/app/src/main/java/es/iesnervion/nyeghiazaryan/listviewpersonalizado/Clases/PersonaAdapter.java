4d73362e23d704a3bd14f3176e1bc51e096ee3ee
nzhdeh21 <nzhdeh1991@gmail.com>
2019-10-21 00:43:05 +0200

    jhghj

--- ListViewPersonalizado/app/src/main/java/es/iesnervion/nyeghiazaryan/listviewpersonalizado/Clases/PersonaAdapter.java
+++ ListViewPersonalizado/app/src/main/java/es/iesnervion/nyeghiazaryan/listviewpersonalizado/Clases/PersonaAdapter.java
@@ -2,62 +2,29 @@ package es.iesnervion.nyeghiazaryan.listviewpersonalizado.Clases;
 
 import android.app.Person;
 import android.content.Context;
-import android.database.DataSetObserver;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Adapter;
 
-import java.util.ArrayList;
-
-import es.iesnervion.nyeghiazaryan.listviewpersonalizado.R;
-
-public class PersonaAdapter extends BaseAdapter
+public class PersonaAdapter extends Adapter<Persona>
 {
      Context contexto;
-     ArrayList <Persona> personas=null;
+     int layoutResourceID;
+     Persona [] personas=null;
 
-    public PersonaAdapter(Context contexto,ArrayList<Persona> personas)
+    public PersonaAdapter(Context contexto,int layoutResourceID,Persona [] personas)
     {
-        //super();
+        super(contexto,layoutResourceID,personas);
         this.contexto=contexto;
+        this.layoutResourceID=layoutResourceID;
         this.personas=personas;
-    }
-
-    @Override
-    public int getCount()
-    {
-        return personas.size();
-    }
-
-    @Override
-    public Object getItem(int position)
-    {
-        return personas.get(position);
-    }
-
-    @Override
-    public long getItemId(int position)
-    {
-        return 0;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        Persona p= (Persona) getItem(position);
-        convertView= LayoutInflater.from(contexto).inflate(R.layout.list_item_row,null);
+        View vista=convertView;
 
-        ImageView foto=convertView.findViewById(R.id.imagen);
-        TextView nombre=convertView.findViewById(R.id.tv1);
-        //TextView apellidos=convertView.findViewById(R.id.tv2);
-
-        foto.setImageResource(p.getImagen());
-        nombre.setText(p.getNombre());
-        //apellidos.setText(p.getApellidos());
-
-        return convertView;
+        return null;
     }
 }
--- ListViewPersonalizado/app/src/main/java/es/iesnervion/nyeghiazaryan/listviewpersonalizado/MainActivity.java
+++ ListViewPersonalizado/app/src/main/java/es/iesnervion/nyeghiazaryan/listviewpersonalizado/MainActivity.java
@@ -3,47 +3,14 @@ package es.iesnervion.nyeghiazaryan.listviewpersonalizado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-
-import es.iesnervion.nyeghiazaryan.listviewpersonalizado.Clases.Persona;
-import es.iesnervion.nyeghiazaryan.listviewpersonalizado.Clases.PersonaAdapter;
 
 public class MainActivity extends AppCompatActivity
 {
-    private ListView lvItem;
-    private PersonaAdapter adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        lvItem=findViewById(R.id.lv1);
-//        adaptador=new PersonaAdapter(this,Lista());
-//        lvItem.setAdapter(adaptador);
-    }
-
-    private ArrayList<Persona> Lista()
-    {
-        ArrayList<Persona> p=new ArrayList<>();
-
-        p.add(new Persona("Nzhdeh",R.drawable.ic_launcher));
-        p.add(new Persona("Nzhdeh",R.drawable.ic_launcher));
-        p.add(new Persona("Nzhdeh",R.drawable.ic_launcher));
-        p.add(new Persona("Nzhdeh",R.drawable.ic_launcher));
-        p.add(new Persona("Nzhdeh",R.drawable.ic_launcher));
-        p.add(new Persona("Nzhdeh",R.drawable.ic_launcher));
-        p.add(new Persona("Nzhdeh",R.drawable.ic_launcher));
-        p.add(new Persona("Nzhdeh",R.drawable.ic_launcher));
-        p.add(new Persona("Nzhdeh",R.drawable.ic_launcher));
-        p.add(new Persona("Nzhdeh",R.drawable.ic_launcher));
-        p.add(new Persona("Nzhdeh",R.drawable.ic_launcher));
-        p.add(new Persona("Nzhdeh",R.drawable.ic_launcher));
-
-
-        return p;
     }
 }
