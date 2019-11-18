105d0c65fd0412c1bf2d5b91beeff2553ecb93ab
Nzhdeh <nzhdeh1991@gmail.com>
2019-11-18 10:06:56 +0100

    vamo a chatea

---
+++ Vamo a Chatea/app/src/main/java/es/iesnervion/nyeghiazaryan/vamoachatea/MainActivity.java
@@ -0,0 +1,37 @@
+package es.iesnervion.nyeghiazaryan.vamoachatea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private EditText usuario;
+    private EditText contraseña;
+    private Button entrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usuario.findViewById(R.id.etUsuario);
+        contraseña.findViewById(R.id.etContraseña);
+        entrar.findViewById(R.id.btnEntrar);
+
+        entrar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(MainActivity.this, "Entrando...", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
